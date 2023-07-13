5598c0b841997135ac7814794cef29a771bc2414
Vinícius Gomes <viniciusgbrb@gmail.com>
2023-07-13 15:31:05 -0300

    atualizando dia 13/

---
+++ fundamentos-01/s3-introducao-a-javascript/d1-primeiros-passos-em-js/7.aula_ao_vivo.js
@@ -0,0 +1,48 @@
+// switch case - adiciona par ou impar e exemplo de switch case
+
+const round = 2; //Entre 1 e 16
+let money = 1200;
+const killWeapon = 'Knife'; //'Knife', 'AWP', 'Desert Eagle', 'Molotov', 'MP7'. 
+const win = true; //false
+const howWin = 'Bomb explode'; //'Eliminando', 'Tempo esgotado', 'Bomb has been planted', 'Bomb has been defused', 'Bomb explode'.
+const defuseBomb = true; //false
+const bombDetonate = false; //true
+
+if (round === 1 || round === 16) { 
+  money = 800;
+} else if (win === true && (howWin === 'Eliminando' || howWin === 'Tempo esgotado')){
+  money += 3250;
+} else if (win === true && howWin === 'Bomb has been planted') {
+  money += 3550;
+} else if (win === true && (howWin === 'Bomb has been defused' || howWin === 'Bomb explode')) {
+  money += 3500;
+} 
+
+if(round !== 1 && round !== 16) {
+  switch (killWeapon) {
+    case 'Knife':
+      money += 1500;
+      break;
+    case 'AWP':
+      money += 100;
+      break;
+    case 'Desert Eagle':
+      money += 300;
+      break;
+    case 'Molotov':
+      money += 300;
+      break;
+    case 'MP7':
+      money += 600;
+      break;
+    default:
+      console.log('Arma não encontrada.');
+      break;
+  }  
+}
+
+if(money > 16000){
+  money = 16000;
+}
+
+console.log('Money: $' + money);
--- fundamentos-01/s3-introducao-a-javascript/d2-array-e-estruturas-de-repetição/3.estrutura_de_repeticao.js
+++ fundamentos-01/s3-introducao-a-javascript/d2-array-e-estruturas-de-repetição/3.estrutura_de_repeticao.js
@@ -14,7 +14,8 @@ console.log('Acabou a corrida!');
 const numbers = [2, 19, 23, 4, 8, 10];
 
 // Variável complementar que irá acumular os valores da somados
-let sum = 0;
+
+let sum = 0; 
 
 // Loop for, que irá iterar da posição 0 até o último elemento do array
 // A variável sum sempre irá receber o valor atual dela + o número do array
@@ -25,11 +26,33 @@ for (let index = 0; index < numbers.length; index += 1) {
 
 console.log(sum);
 
-// ----------------------------------------------------------------------------------------
+// for com template literals ----------------------------------------------------------------------------------------
 
 const shopp = ['sabão', 'macarrão', 'leite'];
 
-for (let index = 0; index < shopp; index += 1) {
-    console.log(index);
-}; // não ta rodando. 
+for (let index = 0; index < shopp.length; index += 1) {
+    // console.log(index); - mostra em lista de numeros
+    // console.log(shopp[index]); - mostra em lista de compras
+    console.log(`${index} - ${shopp[index]}`);
+
+};
+
+// fazendo log de um array de strings ----------------------------------------------------------------------------------------- 
+
+const cars = ['Saab', 'Volvo', 'BMW', 'Chevrolet'];
+
+for (let index = 0; index < cars.length; index += 1) {
+  const currentCar = cars [index];
+  console.log(currentCar);
+}
+
+// exercicio ---- Utilize o for para imprimir os elementos da lista groceryList através de console.log(): ------------------------------------------------------------------------------------- 
+
+const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+  console.log(`${index} - ${groceryList[index]}`);
+}
+
+// aula ao vivo ------------------------------------------------------------------------------
 
